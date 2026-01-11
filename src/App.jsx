a10380d8c079973a6d8c9dc1c721b310c1e853af
Eagle-Nikhil <your-email@example.com>
2026-01-11 15:06:15 +0530

    Add Nikhil portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Khushi from "./pages/Khushi";
+import Nikhil from "./pages/Nikhil";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/khushi" element={<Khushi />} />
+      <Route path="/nikhil" element={<Nikhil />} />
     </Routes>
   );
 }
